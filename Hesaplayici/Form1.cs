a6f3707fc429cd9a09ea2ac11c0d37777a135de5
haluk <98halook@gmail.com>
2025-07-11 12:30:45 +0300

    initial commit: pitch calculator

---
+++ Hesaplayici/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hesaplayici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string panelHeightDifference = textBox1.Text; //Gets panel height diffrence value from textBox1
+
+            double minimumSolarElevationAngleDeg = Convert.ToDouble(textBox2.Text) //Gets minimum solar elevation angle value from textBox2 and covnerts the string to double
+           , angleRad = minimumSolarElevationAngleDeg * Math.PI / 180, tanAngle = Math.Tan(angleRad); 
+
+            double pitchDistance = Convert.ToDouble(panelHeightDifference) / tanAngle; //Calculates the pitch distance by the formula ( pitch = (rear-to-front panel height difference) / tan(minimum solar elevation angle) )
+            
+            textBox3.Text = pitchDistance.ToString("F4"); //Displays the pitch distance rounded to 4 decimal places
+
+        }
+    }
+}
---
+++ Hesaplayici/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Hesaplayici")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hesaplayici")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("926f7a72-1e14-48b3-a57c-8425ac0308db")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
